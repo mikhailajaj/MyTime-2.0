93acf36ba6a2743288f8900e2257045e1fd7b695
mikhail.ajaj <michelajaj96@gmail.com>
2020-11-22 15:26:56 -0500

    Initial Commit

---
+++ MyTime 2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyTime 2.0
+//
+//  Created by Mikhail on 2020-11-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
